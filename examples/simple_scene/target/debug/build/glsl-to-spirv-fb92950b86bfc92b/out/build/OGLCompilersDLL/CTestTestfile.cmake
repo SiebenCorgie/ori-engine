171e33d051e3384ad0699f698a9891cd817d3019
Siebencorgie <siebencorgie@googlemail.com>
2017-07-28 00:26:15 +0200

    removed useless target folder from example
    Former-commit-id: be646f566a186f2f99bbb04620c6c54e0788763b

--- examples/simple_scene/target/debug/build/glsl-to-spirv-fb92950b86bfc92b/out/build/OGLCompilersDLL/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/siebencorgie/Scripts/Rust/engine/ori_engine/lib/vulkano/glsl-to-spirv/glslang/OGLCompilersDLL
-# Build directory: /home/siebencorgie/Scripts/Rust/engine/ori_engine/examples/simple_scene/target/debug/build/glsl-to-spirv-fb92950b86bfc92b/out/build/OGLCompilersDLL
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

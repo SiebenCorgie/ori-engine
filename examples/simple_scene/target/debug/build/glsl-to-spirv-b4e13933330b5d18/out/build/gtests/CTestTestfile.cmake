be646f566a186f2f99bbb04620c6c54e0788763b
Siebencorgie <siebencorgie@googlemail.com>
2017-07-28 00:26:15 +0200

    removed useless target folder from example

--- examples/simple_scene/target/debug/build/glsl-to-spirv-b4e13933330b5d18/out/build/gtests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/siebencorgie/.cargo/registry/src/github.com-1ecc6299db9ec823/glsl-to-spirv-0.1.3/glslang/gtests
-# Build directory: /home/siebencorgie/Scripts/Rust/engine/ori_engine/examples/simple_scene/target/debug/build/glsl-to-spirv-b4e13933330b5d18/out/build/gtests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

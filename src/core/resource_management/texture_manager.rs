502b47c6130af30e22893942413faa438d03ea5b
Siebencorgie <siebencorgie@googlemail.com>
2017-08-16 17:35:54 +0200

    added a prototype `TextureManger` to be used for: https://github.com/SiebenCorgie/ori-engine/issues/2
    Former-commit-id: 5381f17174f8a5fefe376eb96fef010b39979e78

---
+++ src/core/resource_management/texture_manager.rs
@@ -0,0 +1,144 @@
+use std::sync::{Arc, Mutex};
+
+use core::resources::texture;
+use core::engine_settings;
+
+use vulkano::device::{Device, Queue};
+
+
+///Holds a vector of different textures which can be retrieved as a copy in for of a Arc<Texture>
+pub struct TextureManager {
+    //the textures
+    textures: Vec<Arc<texture::Texture>>,
+
+    //Some more copys of structures needed to create more textures
+    device: Arc<Device>,
+    queue: Arc<Queue>,
+    engine_settings: Arc<Mutex<engine_settings::EngineSettings>>
+}
+
+
+impl TextureManager{
+    ///Creates a new Texture manager with two textures:
+    ///A `none` texture, containing a single black pixel, which can be use d for instance if
+    ///you are only using the `material_factor_*` values in a material.
+    ///
+    ///And a `default_texture`, which usually is used if the engine can't find a material or texture
+    /// the `default_texture` comes as `albedo`, `normal`, and `physical` variant
+    pub fn new(
+        device: Arc<Device>,
+        queue: Arc<Queue>,
+        engine_settings: Arc<Mutex<engine_settings::EngineSettings>>
+    ) -> Self{
+
+
+        //The default texture
+        let none_texture = texture::TextureBuilder::from_image(
+            "/home/siebencorgie/Scripts/Rust/engine/ori-engine/data/nothing.png",
+            device.clone(),
+            queue.clone(),
+            engine_settings.clone()
+        )
+        .build_with_name("none");
+
+        //The fallback textures
+        let fallback_albedo = texture::TextureBuilder::from_image(
+            "/home/siebencorgie/Scripts/Rust/engine/ori-engine/data/fallback_alb.png",
+            device.clone(),
+            queue.clone(),
+            engine_settings.clone()
+        )
+        .build_with_name("fallback_albedo");
+
+        let fallback_normal = texture::TextureBuilder::from_image(
+            "/home/siebencorgie/Scripts/Rust/engine/ori-engine/data/fallback_nrm.png",
+            device.clone(),
+            queue.clone(),
+            engine_settings.clone()
+        )
+        .build_with_name("fallback_normal");
+
+        let fallback_physical = texture::TextureBuilder::from_image(
+            "/home/siebencorgie/Scripts/Rust/engine/ori-engine/data/fallback_physical.png",
+            device.clone(),
+            queue.clone(),
+            engine_settings.clone()
+        )
+        .build_with_name("fallback_physical");
+
+        //Now store all the textures
+        let mut current_textures = Vec::new();
+
+        current_textures.push(none_texture);
+        current_textures.push(fallback_albedo);
+        current_textures.push(fallback_normal);
+        current_textures.push(fallback_physical);
+
+        //Create the struct and return it
+        TextureManager{
+            textures: current_textures,
+            //Some more copys of structures needed to create more textures
+            device: device.clone(),
+            queue: queue.clone(),
+            engine_settings: engine_settings.clone()
+        }
+    }
+
+    ///Returns a texture if this name, if not found, returns th fallback texture
+    pub fn get_texture(&mut self, name: &str) -> Arc<texture::Texture>{
+
+        for i in self.textures.iter(){
+            if i.name == String::from(name.clone()){
+                return i.clone();
+            }
+        }
+        //if no texture was found return the fallback albedo (this should always be 1)
+        println!(
+            "WARNING: TEXTURE_MANAGER: Returning fallback albedo because: {} was not found",
+            name.clone()
+        );
+        self.textures[1] .clone()
+    }
+
+    ///Adds a new texture to the manager, this will return an error if the texture is already in
+    ///the manager
+    pub fn add_texture(&mut self, texture: Arc<texture::Texture>) -> Result<(), &'static str>{
+
+        //get the texture name and test the already used textures
+        let name = texture.name.clone();
+
+        for i in self.textures.iter(){
+            if i.name == String::from(name.clone()){
+                return Err("This textuer is already in th manager")
+            }
+        }
+
+        //If the texture passed the name test, we can add it
+        self.textures.push(texture);
+        Ok({})
+    }
+
+    ///Removes the texture from the manager, keep in mind that any copy will live till it is droped
+    ///by its holder.
+    pub fn remove_texture(mut self, texture_name: &str) -> Result<(), &'static str>{
+
+        //Test if the texture is in the naming index
+        let mut texture_index = 0;
+        let mut is_in_manager = false;
+        for i in self.textures.iter_mut(){
+            if i.name == String::from(texture_name){
+                is_in_manager = true;
+                break;
+            }
+            texture_index += 1;
+        }
+        //if the texture was in the manager we can use the last index to remvoe this texture
+        if is_in_manager{
+            self.textures.remove(texture_index);
+            return Ok({});
+        }
+
+        //if the texture was not in the manager, send an error
+        Err("the texture could not removed because it is not in the manager")
+    }
+}
